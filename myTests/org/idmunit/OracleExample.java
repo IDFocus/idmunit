25a94e2b8137155ceba82fa09f2886d6f4898e45
bkynaston <bkynaston@821a3c19-b526-0410-9e98-b9a8be5e021c>
2007-02-21 04:21:24 +0000

    Added example test spreadsheet for Oracle interface.  Refer to the updated idmunit-config.xml file to see the associated connection definition within the trivirLab profile.

---
+++ myTests/org/idmunit/OracleExample.java
@@ -0,0 +1,23 @@
+/**************************************************************************
+ *  IdMUnit - Automated Testing Framework for Identity Management Solutions
+ *
+ * Purpose of this file: This is a sample test runner that provides an interface for
+ * the selection and execution of a spreadsheet and the sheets therein.
+ * 
+ * @author Brent Kynaston, Software Engineer, TriVir LLC
+ *
+ *******************************************************************************/
+package org.idmunit;
+
+public class OracleExample extends IdMUnitTestCase {
+	/* TO BE MODIFIED BY IDMUNIT USER
+	 * These test methods should correspond to the sheets in the spreadsheet 
+	 * that corrsponds to the name of this test class
+	 */
+
+	public void testUserAdd() throws Exception {executeTest();}
+
+	public static Test suite() {
+		return DDStepsSuiteFactory.createSuite(OracleExample.class);
+	}
+}
