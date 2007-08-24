b54013d1a52ad943040240216c89a5db6fed4581
bkynaston <bkynaston@821a3c19-b526-0410-9e98-b9a8be5e021c>
2007-08-24 13:35:25 +0000

    Updated naming conventions and removed non-EAP files

--- myTests/org/idmunit/IMFADTests.java
+++ myTests/org/idmunit/IMFADTests.java
@@ -13,13 +13,13 @@ import junit.framework.Test;
 
 import org.ddsteps.junit.suite.DDStepsSuiteFactory;
 
-public class TrivirDTFDriverTests extends IdMUnitTestCase {
+public class IMFADTests extends IdMUnitTestCase {
 	/* TO BE MODIFIED BY IDMUNIT USER */
 	public void testUserAdd() throws Exception {executeTest();}
 
 	
 	//Used for operational purposes - ensure that the classname used is the same as the name of this class
 	public static Test suite() {
-		return DDStepsSuiteFactory.createSuite(TrivirDTFDriverTests.class);
+		return DDStepsSuiteFactory.createSuite(IMFADTests.class);
 	}
 }
---
+++ myTests/org/idmunit/PPLSFTtoEAP.java
@@ -0,0 +1,27 @@
+/**************************************************************************
+ *  IdMUnit - Automated Testing Framework for Identity Management Solutions
+ *
+ * Purpose of this file: This is a sample test runner that provides an interface for
+ * the selection and execution of a spreadsheet and the sheets therein.
+ * 
+ * @author Brent Kynaston, Software Engineer, TriVir LLC
+ *
+ *******************************************************************************/
+package org.idmunit;
+
+import junit.framework.Test;
+
+import org.ddsteps.junit.suite.DDStepsSuiteFactory;
+
+public class PPLSFTtoEAP extends IdMUnitTestCase {
+	/* TO BE MODIFIED BY IDMUNIT USER */
+	public void testDestroyFixture() throws Exception {executeTest();}
+	public void testSetupFixture() throws Exception {executeTest();}
+/*	public void testEntitlementAdd() throws Exception {executeTest();}
+	public void testEntitlementMod() throws Exception {executeTest();}
+	public void testEntitlementDel() throws Exception {executeTest();}
+*/
+	public static Test suite() {
+		return DDStepsSuiteFactory.createSuite(PPLSFTtoEAP.class);
+	}
+}
